c010288538179d06b63e1c155b326363c01884c1
Thishara Dilmini <dilminithishara@gmail.com>
2026-06-27 15:32:05 +0530

    Add additional JavaScript functionality and implement array logging

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ let name = "Saman";
 console.log(name);
 
 alert("Hello icet");
+
+console.log(typeof name);
+
+
+function add(x,y){
+    return x+y;
+}
+
+let result = add(10,20);
+console.log(result);
+
+//Array
+let numbers = [1 , true , "Kamal" , 3.14];
+
+for(let i=0; i<numbers.length; i++){
+    console.log(numbers[i]);
+}
